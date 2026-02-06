a9d8b319fbcb709563b97741cf7d7116ce42d73f
Leen Aboukhalil <109052421+Leen-ak@users.noreply.github.com>
2026-02-05 21:13:20 -0500

    Adds Material UI and updates App.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
 
+//.render() -> The starting point for the application to start
+// .render() ->  render a React Element (created internally with the React.createElement() function).
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
